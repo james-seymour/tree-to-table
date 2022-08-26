8a309b2dd0e2dace948dee0377d418aea4de87c9
James Seymour <james.seymour@cubiko.com.au>
2022-08-27 08:50:57 +1000

    rename run -> apply

--- tree_to_table/core/mappers/basic.py
+++ tree_to_table/core/mappers/basic.py
@@ -21,12 +21,23 @@ class GetDefault(Mapper[Dict[str, Result], Result, FinalResult]):
         return obj.get(self.key, self.default)
 
 
+class Nested(Mapper[Source, Source, Dict[str, FinalResult]]):
+    def __init__(self, nexts: Dict[str, Callable[[Source], FinalResult]]) -> None:
+        self.nexts = nexts
+
+    def apply(self, obj: Source) -> Source:
+        return obj
+
+    def __call__(self, obj) -> Dict[str, FinalResult]:
+        return {key: mapper(obj) for key, mapper in self.nexts.items()}
+
+
 class Apply(Mapper):
     def __init__(self, apply_fn: Callable, next=identity):
         self.apply_fn = apply_fn
         super().__init__(next)
 
-    def run(self, obj: Any):
+    def apply(self, obj: Any):
         return self.apply_fn(obj)
 
 
@@ -35,7 +46,7 @@ class Choice(Mapper):
         self.chooser = chooser
         super().__init__(next)
 
-    def run(self, obj):
+    def apply(self, obj):
         if obj:
             return self.chooser(obj)
         return None
@@ -46,5 +57,5 @@ class Replace(Mapper):
         self.replacement = replacement
         super().__init__(next)
 
-    def run(self, _):
+    def apply(self, _):
         return self.replacement
--- tree_to_table/core/mappers/extension.py
+++ tree_to_table/core/mappers/extension.py
@@ -5,10 +5,7 @@ class Coalesce(Mapper):
     def __init__(self, *mappers):
         self.mappers = mappers
 
-    def __call__(self, obj):
-        return self.run(obj)
-
-    def run(self, obj):
+    def apply(self, obj):
         for mapper in self.mappers:
             result = mapper(obj)
             if result:
@@ -25,9 +22,6 @@ class ContextChoice(Mapper):
         self.context = context
         self.mapper = mapper
 
-    def __call__(self, obj):
-        return self.run(obj)
-
-    def run(self, obj):
+    def apply(self, obj):
         realised_context = {name: mapper(obj) for name, mapper in self.context.items()}
         return self.mapper(realised_context)(obj)
