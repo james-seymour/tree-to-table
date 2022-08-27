ef80556497eead865551d8ad8761527f527ca799
James Seymour <james.seymour@cubiko.com.au>
2022-08-27 12:52:59 +1000

    multiple one -> many transform test

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -8,10 +8,83 @@ logger = logging.getLogger()
 
 
 class TestOneToManyTransform:
-    data = [{"split_key": [1, 2, 3], "other_key": 10}]
+    data = [
+        {
+            "patient_id": 69,
+            "practitioner_id": 420,
+            "service_ids": [1, 2, 3],
+            "items_billed": [104, 105, 105],
+            "amounts_billed": [45, 60, 75],
+        },
+        {
+            "patient_id": 100,
+            "practitioner_id": 421,
+            "service_ids": [4, 5, 6],
+            "items_billed": [10945, 2721, 932],
+            "amounts_billed": [100, 120, 140],
+        },
+    ]
 
-    def test_simple_one_to_many(self):
-        logger.info("hello")
-        split_mappers = {"splitted": Get("split_key")}
-        mappers = {"other": Get("other_key")}
-        logger.debug(OneToManyTransform(split_mappers)(mappers, self.data))
+    def test_multiple_one_to_many(self):
+        split_mappers = {
+            "service_id": Get("service_ids"),
+            "item_billed": Get("items_billed"),
+            "amount_billed": Get("amounts_billed"),
+        }
+
+        mappers = {
+            "patient_id": Get("patient_id"),
+            "practitioner_id": Get("practitioner_id"),
+        }
+
+        transformed_generator = OneToManyTransform(split_mappers, keep_splits=True)(
+            mappers, self.data
+        )
+        transformed = list(transformed_generator)
+
+        transformed_expected = [
+            {
+                "patient_id": 69,
+                "practitioner_id": 420,
+                "service_id": 1,
+                "item_billed": 104,
+                "amount_billed": 45,
+            },
+            {
+                "patient_id": 69,
+                "practitioner_id": 420,
+                "service_id": 2,
+                "item_billed": 105,
+                "amount_billed": 60,
+            },
+            {
+                "patient_id": 69,
+                "practitioner_id": 420,
+                "service_id": 3,
+                "item_billed": 105,
+                "amount_billed": 75,
+            },
+            {
+                "patient_id": 100,
+                "practitioner_id": 421,
+                "service_id": 4,
+                "item_billed": 10945,
+                "amount_billed": 100,
+            },
+            {
+                "patient_id": 100,
+                "practitioner_id": 421,
+                "service_id": 5,
+                "item_billed": 2721,
+                "amount_billed": 120,
+            },
+            {
+                "patient_id": 100,
+                "practitioner_id": 421,
+                "service_id": 6,
+                "item_billed": 932,
+                "amount_billed": 140,
+            },
+        ]
+
+        assert transformed == transformed_expected
